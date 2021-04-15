ecdba33210d18a0e81abc8cee6af0933b0f716a9
oliver <metreo@sent.com>
2021-04-15 13:02:30 -0300

    update

---
+++ model_composition.jl
@@ -0,0 +1,18 @@
+# based on 
+using Soss
+using Soss: Dists
+
+m1 = @model begin
+  x1 ~ Soss.Normal(0.0,1.0)
+  x2 ~ Dists.LogNormal(0.0,1.0)
+  return x1^2/x2
+end
+
+m2 = @model m begin
+  μ ~ m
+  y ~ Soss.Normal(μ, 1.0)
+end
+
+mm = m2(m=m1())
+println(mm)
+println(xform(mm|(t=1.0,)))
