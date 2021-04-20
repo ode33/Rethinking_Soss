3a7f735b5950b60b1f9a35b4a8b9eff1d4ea618b
oliver <metreo@sent.com>
2021-04-20 15:27:53 -0300

    update

--- model_composition.jl
+++ model_composition.jl
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 # taken from https://github.com/cscherrer/Soss.jl/issues/258
-=======
-# based on 
->>>>>>> ecdba33210d18a0e81abc8cee6af0933b0f716a9
 using Soss
 using Soss: Dists
 
 m1 = @model begin
-<<<<<<< HEAD
     x1 ~ Soss.Normal(0.0, 1.0)
     x2 ~ Dists.LogNormal(0.0, 1.0)
     return x1^2/x2
@@ -19,7 +14,6 @@ m2 = @model m begin
 end
 
 println(xform(m2(m=m1())|(y=1.0,)))
-=======
   x1 ~ Soss.Normal(0.0,1.0)
   x2 ~ Dists.LogNormal(0.0,1.0)
   return x1^2/x2
@@ -33,4 +27,3 @@ end
 mm = m2(m=m1())
 println(mm)
 println(xform(mm|(t=1.0,)))
->>>>>>> ecdba33210d18a0e81abc8cee6af0933b0f716a9
